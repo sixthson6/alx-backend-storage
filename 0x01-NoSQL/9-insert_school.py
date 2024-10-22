574f58dce6f67bb1e6c08f3d0002ec08bcf3257d
sixthson6 <sudais2408@gmail.com>
2024-10-22 19:06:26 +0000

    insert into db (pymongo)

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""insert into db"""
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert into db"""
+    return mongo_collection.insert_one(kwargs)._id
