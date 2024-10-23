ef0bb72c85b3712930ada1b9094c9099f90b34b6
sixthson6 <sudais2408@gmail.com>
2024-10-23 00:48:24 +0000

    redis setup

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
 
-    def store(self, data: str) -> str:
+    def store(self, data: str) -> str | None:
         """Store"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
